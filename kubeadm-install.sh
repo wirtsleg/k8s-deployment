a3a27afbd27c2a66c244eb846d721144de1c08e3
root <shuto4ka1@gmail.com>
2018-10-10 18:09:35 +0000

    Added docker install

--- kubeadm-install.sh
+++ kubeadm-install.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-apt-get update && apt-get install -y apt-transport-https curl
+apt-get update && apt-get install -y apt-transport-https curl software-properties-common
+curl -fsSL 'https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | apt-key add -
+add-apt-repository \
+   "deb https://packages.docker.com/1.12/apt/repo/ \
+   ubuntu-$(lsb_release -cs) \
+   main"
+apt-get update
+apt-get -y install docker-engine
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
@@ -8,3 +16,6 @@ EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
+
+docker info
+kubectl version
