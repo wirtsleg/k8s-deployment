38e851924b1b7986c2da24e17ed390af8ef38cc7
shuto4ka <shuto4ka1@gmail.com>
2018-10-13 14:33:12 +0300

    Fixed docker install

--- kubeadm-install.sh
+++ kubeadm-install.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-apt-get update && apt-get install -y apt-transport-https curl software-properties-common
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
-snap install docker
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt-cache policy docker-ce
+apt install docker-ce
+systemctl status docker
 
+# Install k8s tools
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
